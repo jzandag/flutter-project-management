cc24eafb655188dc319cd2e37d8f177a37cd7287
jzandag <zidrexandag10@gmail.com>
2022-05-22 15:03:23 +0800

    initial commit w/ layout for auth screens

---
+++ android/app/src/main/java/com/jzandag/flutter_project_management/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jzandag.flutter_project_management;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  // App Settings
+  static const String appTitle = "Project Management App";
+
+  // Color constants
+
+  // Text style constants
+
+  // Appbar theme data
+}
